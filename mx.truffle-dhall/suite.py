6564abac3638f99652c9fbf88152b7b6461ee041
Alexander Ronald Altman <alexanderaltman@me.com>
2020-03-21 22:14:17 -0700

    Update suite.py

--- mx.truffle-dhall/suite.py
+++ mx.truffle-dhall/suite.py
@@ -6,6 +6,18 @@ suite = {
     ]
   },
   "libraries" : {
+    "ANTLR4": {
+      "sha1" : "7fe453b678b71c87c6677cef26f6b0cd96b26586",
+      "maven" : {
+        "groupId" : "org.antlr",
+        "artifactId" : "antlr4-runtime",
+        "version" : "4.8",
+      }
+    },
+    "ANTLR4_COMPLETE": {
+      "urls": ["https://www.antlr.org/download/antlr-4.8-complete.jar"],
+      "sha1": "a675db077abf60d720f80f968602be763131820c",
+    },
   },
   "projects" : {
   },
