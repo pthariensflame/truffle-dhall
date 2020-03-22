e140cbd29a318a7929ac18be64ac613d1fc300ea
Alexander Ronald Altman <alexanderaltman@me.com>
2020-03-21 23:05:50 -0700

    Update suite.py

--- mx.truffle-dhall/suite.py
+++ mx.truffle-dhall/suite.py
@@ -9,7 +9,7 @@ suite = {
     "organization" : "Oracle Corporation",
     "organizationUrl" : "http://www.graalvm.org/",
   },
-  "defaultLicense" : "BSD-3",
+  "defaultLicense" : "BSD-3-Clause",
   "scm" : {
     "url" : "https://github.com/pthariensflame/truffle-dhall",
     "read" : "https://github.com/pthariensflame/truffle-dhall.git",
@@ -28,6 +28,14 @@ suite = {
         "version" : "3.14.0",
       }
     },
+    "JLINE_JANSI" : {
+      "sha1" : "70ab53c1c3d610392905835ed5ecfebb0381c334",
+      "maven" : {
+        "groupId" : "org.jline",
+        "artifactId" : "jline-terminal-jansi",
+        "version" : "3.14.0",
+      }
+    },
     "ANTLR4": {
       "sha1" : "7fe453b678b71c87c6677cef26f6b0cd96b26586",
       "maven" : {
@@ -39,6 +47,22 @@ suite = {
     "ANTLR4_COMPLETE": {
       "urls": ["https://www.antlr.org/download/antlr-4.8-complete.jar"],
       "sha1": "a675db077abf60d720f80f968602be763131820c",
+    },
+    "GRAALVM_SDK" : {
+      "sha1" : "8ad38d2054bf7c56d1b0286c821117ddbfd5c4a4",
+      "maven" : {
+        "groupId" : "org.graalvm.sdk",
+        "artifactId" : "graal-sdk",
+        "version" : "20.0.0",
+      }
+    },
+    "TRUFFLE_API" : {
+      "sha1" : "babdf30d5bc62bcce62dee85bd85f42e03841693",
+      "maven" : {
+        "groupId" : "org.graalvm.truffle",
+        "artifactId" : "truffle-api",
+        "version" : "20.0.0",
+      }
     },
   },
   "projects" : {
