be255e4045453b5591eef1c497b2d6ad9d441cc6
Alexander Ronald Altman <alexanderaltman@me.com>
2020-03-25 23:22:04 -0700

    More rework

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,14 @@
+/**
+ * @author Alexander Ronald Altman
+ */
 module com.pthariensflame.truffle_dhall {
 	exports com.pthariensflame.truffle_dhall;
+	exports com.pthariensflame.truffle_dhall.evaluation;
 	exports com.pthariensflame.truffle_dhall.shell;
 	exports com.pthariensflame.truffle_dhall.parser;
-	exports com.pthariensflame.truffle_dhall.parser.antlr;
 
-	requires org.antlr.v4.runtime;
+	//requires antlr4.runtime;
 	requires org.graalvm.truffle;
 	requires org.graalvm.sdk;
+	requires com.pthariensflame.truffle_dhall.parser.antlr;
 }
